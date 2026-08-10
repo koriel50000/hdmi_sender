b4b328426fb4dcddf1f2cb637fcdb8c11a05161e
koriel50000 <JZW00040@gmail.com>
2026-08-10 22:31:39 +0900

    update rgb

--- sender_hls/C_source/sender.cpp
+++ sender_hls/C_source/sender.cpp
@@ -16,29 +16,27 @@ void pattern_sender (hls::stream<pixel_t> &pin, hls::stream<pixel_t> &pout)
     ap_uint<9> col_x;
     ap_uint<8> col_y;
     const ap_uint<8> zero = 0x00;
+    const ap_uint<8> full = 0xff;
     int frame = 0;
     
-    col_y = (frame >> 1) & 0xff;
     for (int y = 0; y < 720; y++) {
-        col_x = frame & 0x1ff;
         for (int x = 0; x < 1280; x++) {
 #pragma HLS PIPELINE
             pin >> ptmp;
-            if (160 <= x && x < 480 && 160 <= y && y < 320) {
-                p.data.range(23, 16) = (col_x[8]) ? col_y : zero;
-                p.data.range(15,  8) = (col_x[6]) ? col_y : zero;
-                p.data.range( 7,  0) = (col_x[7]) ? col_y : zero;
+            if (160 <= x && x < 640 && 160 <= y && y < 320) {
+                int n = x / 160;
+                p.data.range(23, 16) = (n == 1) ? full : zero; // R
+                p.data.range(15,  8) = (n == 2) ? full : zero; // G
+                p.data.range( 7,  0) = (n == 3) ? full : zero; // B
             } else {
                 ap_uint<24> d = ptmp.data;
-                p.data.range(23, 16) = d.range(23, 16); // R
-                p.data.range(15,  8) = d.range(15,  8); // G
-                p.data.range( 7,  0) = d.range( 7,  0); // B
+                p.data.range(23, 16) = d.range( 7,  0); // R
+                p.data.range(15,  8) = d.range(23, 16); // G
+                p.data.range( 7,  0) = d.range(15,  8); // B
             }
             p.user[0] = (x == 0 && y == 0);
             p.last    = (x == 1279);
             pout << p;
-            col_x++;
         }
-        col_y++;
     }
 }
