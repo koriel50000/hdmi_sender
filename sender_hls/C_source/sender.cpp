8ab6a4ff1fbc119c0fd38ddc46b906c505ff666c
koriel50000 <JZW00040@gmail.com>
2026-08-11 08:36:24 +0900

    update rbg

--- sender_hls/C_source/sender.cpp
+++ sender_hls/C_source/sender.cpp
@@ -30,9 +30,9 @@ void pattern_sender (hls::stream<pixel_t> &pin, hls::stream<pixel_t> &pout)
                 p.data.range( 7,  0) = (n == 3) ? full : zero; // G
             } else {
                 ap_uint<24> d = ptmp.data;
-                p.data.range(23, 16) = d.range( 7,  0); // G
-                p.data.range(15,  8) = d.range(23, 16); // R
-                p.data.range( 7,  0) = d.range(15,  8); // B
+                p.data.range(23, 16) = d.range(23, 16); // R
+                p.data.range(15,  8) = d.range( 7,  0); // B
+                p.data.range( 7,  0) = d.range(15,  8); // G
             }
             p.user[0] = (x == 0 && y == 0);
             p.last    = (x == 1279);
--- sender_hls/_ide/workspace_journal.py
+++ sender_hls/_ide/workspace_journal.py
@@ -4,3 +4,8 @@ import vitis
 client = vitis.create_client()
 client.set_workspace(path="sender_hls")
 
+comp = client.get_component(name="sender_hls")
+comp.run(operation="SYNTHESIS")
+
+comp.run(operation="PACKAGE")
+
